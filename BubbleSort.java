bc241c41430e790ce8565e9e938eaef5eaab357b
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-03 15:54:53 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,16 +1,17 @@
-public int bubbleSort(int[] intArray) {
+public static void bubbleSort(int[] intArray) {
 
-  int i, j, temp = 0;
+        int i, j, temp = 0;
 
-// traversing through the array
-  for(i = intAarray.length-1; i>0; i--){
-    for(j = 0; j<intAarray.length-; j++){
-      if(intArray[j]>intArray[j+1]){
-        // swapping values
-        temp = intArray[j];
-        intArray[j] = intArray[j+1];
-        intArray[j+1] = temp;
-      }
+        // traversing through the array
+        for (i = intArray.length - 1; i > 0; i--) {
+            for (j = 0; j < intArray.length - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    // swapping values
+                    temp = intArray[j];
+                    intArray[j] = intArray[j + 1];
+                    intArray[j + 1] = temp;
+                }
+            }
+        }
+    return ;
     }
-    return list;
-  }
