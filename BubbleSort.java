3c1b67c9c0186d328ad3ab1d87038579e065630a
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-03 14:08:52 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,16 @@
+public int bubbleSort(int[] intArray) {
+
+  int i, j, temp = 0;
+
+// traversing through the array
+  for(i = intAarray.length-1; i>0; i--){
+    for(j = 0; j<intAarray.length-; j++){
+      if(intArray[j]>intArray[j+1]){
+        // swapping values
+        temp = intArray[j];
+        intArray[j] = intArray[j+1];
+        intArray[j+1] = temp;
+      }
+    }
+    return list;
+  }
