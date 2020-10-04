b8f4f6be605740d3bc9149df777de9dd1b658a2a
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-04 11:51:50 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,13 +1,14 @@
 public static void selectionSort(int[] intArray){
-        int i, temp = 0;
-        for(i=intArray.length; i>0; i--){
-            int largest = intArray[i];
-            int j = 1;
-            while(j>0 && intArray[j-1]>largest){
-                intArray[largest] = intArray[j-1];
-                j++;
+        for(int i=intArray.length-1; i>0; i--){
+            int largest = 0;
+            for(int j=1; j<=i; j++){
+                if (intArray[j] > intArray[largest]) {
+                    largest = j;
+                }
             }
+            int temp = intArray[i];
             intArray[i] = intArray[largest];
+            intArray[largest] = temp;
         }
         //return;
     }
