4e3859e714b0865fc29338936b396baf6dc298d8
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-04 12:59:39 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,10 @@
+public static void insertionSort(int[] intArray){
+        for(int i=1; i< intArray.length; i++){
+            int newElement = intArray[i];
+            int j;
+            for(j=i; j>0 && intArray[j-1]>newElement; j--){
+                intArray[j] = intArray[j-1];
+            }
+            intArray[j] = newElement;
+        }
+    }
