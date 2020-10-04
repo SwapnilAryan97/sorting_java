98b0d501525b4ae59870a0505fa7ca0f95c9cc08
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-04 11:55:51 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,11 +1,14 @@
 public static void selectionSort(int[] intArray){
+        // starting from enf of array
         for(int i=intArray.length-1; i>0; i--){
             int largest = 0;
+            // traversing from start to end of array    
             for(int j=1; j<=i; j++){
                 if (intArray[j] > intArray[largest]) {
                     largest = j;
                 }
-            }
+            } 
+            // placing largest value at the end of the array
             int temp = intArray[i];
             intArray[i] = intArray[largest];
             intArray[largest] = temp;
