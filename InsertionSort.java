737fa4437838dd165c07d1bb85b9ea877f8f856b
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-04 13:33:31 +0530

    Stable

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,5 +1,5 @@
 public static void insertionSort(int[] intArray){
-        for(int i=1; i< intArray.length; i++){
+        for(int i=1; i<intArray.length; i++){
             int newElement = intArray[i];
             int j;
             for(j=i; j>0 && intArray[j-1]>newElement; j--){
