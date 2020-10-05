750eb03f9221616e367e0a96be61995619e157b7
Swapnil Aryan Sinha <saiamthere@gmail.com>
2020-10-05 18:08:50 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,8 +3,10 @@ public static void insertionSort(int[] intArray){
             int newElement = intArray[i];
             int j;
             for(j=i; j>0 && intArray[j-1]>newElement; j--){
+                // shifting values larger than newElement to the right
                 intArray[j] = intArray[j-1];
             }
+            // inserting newElement to the left of larger elements
             intArray[j] = newElement;
         }
     }
